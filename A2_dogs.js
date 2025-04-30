4197a802e0f6d5b36a60e5b41514dbcfdd5fa6e7
thenoah54 <thenoah54@gmail.com>
2025-04-30 16:37:55 -0400

    4/30 afternoon

--- A2_dogs.js
+++ A2_dogs.js
@@ -1,9 +1,21 @@
 const path2 = 'https://dogapi.dog/api/v2/breeds';
 
+// Creating an object literal
+const dogdivData = {
+    border: '3px solid black',
+    backgroundColor: '#00ff0080',
+    padding: '10px',
+    maring: '10px',
+    name: null,
+    desc: null,
+    min: null,
+    max: null
+  }
+
 function getDog() {
     const path = 'https://dog.ceo/api/breeds/image/random/10';
     fetch(path).then((result) => result.json()).then((resultJson) => {
-        console.log(resultJson);
+        // console.log(resultJson);
         resultJson.message.forEach((resultData) => {
             // console.log(resultData);
             const slider = document.getElementById('dogSlider');
@@ -20,22 +32,23 @@ function getDog() {
 
         resultJson.data.forEach((resultData) => {
             const dogData = resultData.attributes;
-            console.log(dogData);
+            // console.log(dogData);
             const buttonContainer = document.getElementById('dogButtons');
             const dogButton = document.createElement('button');
+            dogButton.id = dogData.name;
             dogButton.textContent = dogData.name;
             dogButton.className = 'dogClass';
 
             function displayDog() {
                 const codeHeader = document.getElementById('dogInfo');
-                codeHeader.style.border = '3px solid black';
-                codeHeader.style.backgroundColor = '#00ff0080';
-                codeHeader.style.padding = '10px';
-                codeHeader.style.margin = '10px';
-                document.getElementById('dogName').textContent = dogData.name;
-                document.getElementById('dogDesc').textContent = dogData.description;
-                document.getElementById('min').textContent = `Min Life: ${dogData.life.min}`;
-                document.getElementById('max').textContent = `Max Life: ${dogData.life.max}`;
+                codeHeader.style.border = dogdivData.border;
+                codeHeader.style.backgroundColor = dogdivData.backgroundColor;
+                codeHeader.style.padding = dogdivData.padding;
+                codeHeader.style.margin = dogdivData.margin;
+                document.getElementById('dogName').textContent = dogdivData.name = dogData.name;
+                document.getElementById('dogDesc').textContent = dogdivData.desc = dogData.description;
+                document.getElementById('min').textContent = dogdivData.min = `Min Life: ${dogData.life.min}`;
+                document.getElementById('max').textContent = dogdivData.max = `Max Life: ${dogData.life.max}`;
             }
             // button.addEventListener('click', functionName) <-- how to create button func
             dogButton.addEventListener('click', displayDog);
@@ -55,11 +68,32 @@ function audio() {
             window.location.replace(`A2_${page}.html`);
         }
         
+        const changeDog = function(dog) {
+            fetch(path2).then((result) => result.json()).then((resultJson) => {
+                resultJson.data.forEach((resultData) => {
+                    if(dog.toUpperCase().replaceAll(" ", "") == resultData.attributes.name.toUpperCase().replaceAll(" ", "")) {
+                        const codeHeader = document.getElementById('dogInfo');
+                        codeHeader.style.border = dogdivData.border;
+                        codeHeader.style.backgroundColor = dogdivData.backgroundColor;
+                        codeHeader.style.padding = dogdivData.padding;
+                        codeHeader.style.margin = dogdivData.margin; 
+                        document.getElementById('dogName').textContent = dog;
+                        document.getElementById('dogDesc').textContent = resultData.attributes.description;
+                        document.getElementById('min').textContent = `Min Life: ${resultData.attributes.life.min}`;
+                        document.getElementById('max').textContent = `Max Life: ${resultData.attributes.life.max}`;;
+                        
+                    }
+                });
+            });
+            
+        }
 
         const commands = {
             'hello': () => { alert('Hello world!'); },
             'change the color to *color': changeColor,
-            'navigate to *page': changePage    
+            'navigate to *page': changePage,
+            'load dog breed *dog': changeDog
+
         }
         
         // Add our commands to annyang
@@ -69,11 +103,11 @@ function audio() {
 }
 
 function startAudio() {
+    audio();
     annyang.start();
 }
 function endAudio() {
     annyang.abort();
 }
 
-window.onload = audio();
 window.onload = getDog;
